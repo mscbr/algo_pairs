eadd409c9d06191b562423f584daf47bd6c5324c
maciej@swapcard.com <maciej@swapcard.com>
2022-12-03 14:06:57 +0100

    CointAnalyzer coint cache

--- modules/CointAnalyzer.py
+++ modules/CointAnalyzer.py
@@ -17,17 +17,19 @@ class Coint_Analyzer:
     def __init__(
         self,
         raw_data_path="../raw_data/",
-        processed_data_path="../processed/",
         closing_prices_container_paths=[
             "Binance_Historical_15m_FUTURES_20_days_2022-07-14T12:00:43"],
+        processed_data_path="../processed/",
+        cache_path=None,
         interval="15m",
         observations_filter=None,
         days_filter=None,
         corr_filter=None
     ):
         self.raw_data_path = raw_data_path
-        self.processed_data_path = processed_data_path
         self.closing_prices_container_paths = closing_prices_container_paths
+        self.processed_data_path = processed_data_path
+        self.cache_path = cache_path
 
         self.observations_filter = observations_filter
         self.days_filter = days_filter
@@ -87,12 +89,13 @@ class Coint_Analyzer:
         else:
             self.df = df_closings.dropna()
 
-    def generate_co_matrices(self, generate_excel=False, vanilla_kalman=False):
+    def generate_co_matrices(self, generate_excel=False, vanilla_kalman=False, create_cache=False):
         # CORRELATION
         self._get_correlated_pairs(
             generate_excel=generate_excel, corr_filter=self.corr_filter)
         # COINTEGRATION
-        self._get_cointegrated_pairs(vanilla_kalman=vanilla_kalman)
+        self._get_cointegrated_pairs(
+            vanilla_kalman=vanilla_kalman, create_cache=create_cache)
 
     def _get_correlated_pairs(self, generate_excel=False, corr_filter=None):
         corr_matrix = self.df.pct_change().corr(method='pearson')
@@ -124,17 +127,45 @@ class Coint_Analyzer:
         except:
             print("Couldn't save correlated pairs to files")
 
-    def _get_cointegrated_pairs(self, vanilla_kalman=False):
+    def _get_cointegrated_pairs(self, vanilla_kalman=False, create_cache=False):
+        cache = None
+        cache_result = None
         df = self.df.copy()
-        pairs = []
 
+        pairs = []
         corr_pairs_names = [pair.split("-")
                             for pair in list(self.corr_pairs.index)]
+
+        # INIT CACHE HANDLING
+        if create_cache and not self.cache_path:
+            cache = pd.DataFrame(index=list(self.corr_pairs.index),
+                                 data={
+                                     'analyzed': [False],
+                                     'corr': [np.nan],
+                                     'adf': [np.nan],
+                                     'hurst': [np.nan]
+            })
+            cache_path = ("%scoint_anal_cache_%s_%s.csv" %
+                          (self.processed_data_path, self.interval, self.uuid))
+            self.cache_path = cache_path
+        elif self.cache_path:
+            cache = pd.read_csv(self.cache_path, index_col=0)
+        if cache is not None:
+            cache_finished_pairs = list(
+                cache.loc[cache.analyzed == True].index)
+            pairs_to_analyze = [
+                pair for pair in list(self.corr_pairs.index) if pair not in cache_finished_pairs]
+            corr_pairs_names = [pair.split("-")
+                                for pair in pairs_to_analyze]
+
+        # RUNNING ANALYSIS
         for i, corr_pair in enumerate(corr_pairs_names):
             print("Performing coint test %s %s" % (i, len(corr_pairs_names)))
             inst_1 = corr_pair[0]
             inst_2 = corr_pair[1]
             result = coint(df[inst_1], df[inst_2])
+            if cache is not None:
+                cache_result = [np.nan, np.nan, np.nan]
 
             # testing for spread stationarity
             if result[1] < 0.05:
@@ -156,22 +187,48 @@ class Coint_Analyzer:
                         index = "%s-%s" % (inst_1, inst_2)
                         pairs.append(
                             (index, self.corr_pairs.loc[index][0], result_adf[0], hurst))
+                        if cache is not None:
+                            cache_result = [
+                                self.corr_pairs.loc[index][0], result_adf[0], hurst]
+            # UPDATING CACHE
+            if cache is not None:
+                try:
+                    cache.loc["%s-%s" % (inst_1, inst_2)] = [
+                        True,
+                        *cache_result
+                    ]
+                    cache.to_csv(self.cache_path)
+                except:
+                    print("Couldn't write coint cache")
 
         try:
             indexes = []
             corr = []
             adf = []
             hurst = []
-            for row in pairs:
-                indexes.append(row[0])
-                corr.append(row[1])
-                adf.append(row[2])
-                hurst.append(row[3])
+            for column in pairs:
+                indexes.append(column[0])
+                corr.append(column[1])
+                adf.append(column[2])
+                hurst.append(column[3])
+
+            # ADDING CACHED RESULTS
+            if cache is not None:
+                cache.dropna(inplace=True)
+                for i in range(0, len(cache)):
+                    if cache.index[i] not in indexes:
+                        column = cache.iloc[i].tolist()
+                        indexes.append(cache.index[i])
+                        corr.append(column[1])
+                        adf.append(column[2])
+                        hurst.append(column[3])
 
             coint_pairs_df = pd.DataFrame(index=indexes)
             coint_pairs_df['corr'] = corr
             coint_pairs_df['adf'] = adf
             coint_pairs_df['hurst'] = hurst
+            coint_pairs_df.sort_values(
+                by='corr', ascending=False, inplace=True)
             coint_pairs_df.to_csv("%scoint_pairs_%s_%s.csv" %
                                   (self.processed_data_path, self.interval, self.uuid))
 
